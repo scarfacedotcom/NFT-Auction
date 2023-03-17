ca1b12cf39d573ed9a95e8dadc9d6392eb087127
scarfacedotcom <peteradaaku@gmail.com>
2023-03-17 10:14:29 +0100

    bid function

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -15,20 +15,43 @@ contract NFT_Auction {
 
     //events
     event Start();
+    event End(address highestBidder, uint highestBid);
 
     constructor () {
         owner = msg.sender; 
     }
 
-    function start() public external {
+    function start(uint startingBid) public external {
         require(!started, "started");
         require(msg.sender == seller, "you are not the seller");
         started true;
         endAt = block.timeStamp + 7 days;
+        highestBid = startingBid;
 
 
         emit Start();
 
+    }
+
+    function bid() external payable {
+        require(started, "Not started yet");
+        require(!end, "ended");
+        require(msg.value > highestBid, "you're fucking broke");
+        require(block.timeStamp < endAt, "ended");
+
+
+        highestBid = msg.value;
+        highestBidder = msg.sender;
+
+    }
+
+    function end() external {
+        require(started, "auction havent started yet");
+        require(block.timeStamp >= endAt, "auction havent ended yet");
+        require(!ended, "auction already ended");
+        ended true;
+
+        emit(highestBidder, highestBid);
     }
 
 }
