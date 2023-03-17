ede3effaabbb6f2a21185949b15b66ae4c19ab4a
scarfacedotcom <peteradaaku@gmail.com>
2023-03-17 10:54:21 +0100

    events and emits

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.18;
+pragma solidity ^0.8.17;
 
 contract NFT_Auction {
-    address public payable seller;
+    address public seller;
+    address public owner;
 
     bool public ended;
     bool public started;
@@ -16,16 +17,18 @@ contract NFT_Auction {
     //events
     event Start();
     event End(address highestBidder, uint highestBid);
+    event Bid(address indexed sender, uint amount);
+    event Withdraw(address indexed bidder, uint amount);
 
     constructor () {
         owner = msg.sender; 
     }
 
-    function start(uint startingBid) public external {
+    function start(uint startingBid) public {
         require(!started, "started");
         require(msg.sender == seller, "you are not the seller");
-        started true;
-        endAt = block.timeStamp + 7 days;
+        started = true;
+        endAt = block.timestamp + 7 days;
         highestBid = startingBid;
 
 
@@ -35,23 +38,40 @@ contract NFT_Auction {
 
     function bid() external payable {
         require(started, "Not started yet");
-        require(!end, "ended");
+        require(!ended, "ended");
         require(msg.value > highestBid, "you're fucking broke");
-        require(block.timeStamp < endAt, "ended");
+        require(block.timestamp < endAt, "ended");
+
+        if(highestBidder != address(0)) {
+            bids[highestBidder] += highestBid;
+        }
 
 
         highestBid = msg.value;
         highestBidder = msg.sender;
 
+        emit Bid(highestBidder, highestBid);
+
+    }
+
+    function withdraw() external payable {
+        uint bal = bids[msg.sender];
+        bids[msg.sender] = 0;
+        (bool sent, bytes memory data) = payable(msg.sender).call{value: bal}("");
+        require(sent, "could not withdraw");
+
+
+        emit Withdraw(msg.sender, bal);
+
     }
 
     function end() external {
         require(started, "auction havent started yet");
-        require(block.timeStamp >= endAt, "auction havent ended yet");
+        require(block.timestamp >= endAt, "auction havent ended yet");
         require(!ended, "auction already ended");
-        ended true;
+        ended = true;
 
-        emit(highestBidder, highestBid);
+        emit End(highestBidder, highestBid);
     }
 
 }
