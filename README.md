96d27dc83beff0ef8f75517729a2e9789399a11f
scarfacedotcom <peteradaaku@gmail.com>
2023-03-18 18:35:49 +0100

    reas

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 
 This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
 
-#####################AUCTION CONTRACT################################
-Auction Contract has been deployed to 0xdB012DD3E3345e2f8D23c0F3cbCb2D94f430Be8C
+#####################NFT CONTRACT################################
+Successfully submitted source code for contract
+contracts/ScarNFT.sol:AuctionToken at 0x73389519fC47F110940181427dA1338fa9F1007E
+for verification on the block explorer. Waiting for verification result...
+
+Successfully verified contract AuctionToken on Etherscan.
+https://goerli.etherscan.io/address/0x73389519fC47F110940181427dA1338fa9F1007E#code
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,32 @@
+// import { HardhatUserConfig } from "hardhat/config";
+// import "@nomicfoundation/hardhat-toolbox";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// const config: HardhatUserConfig = {
+//   solidity: "0.8.17",
+//   networks: {
+//     hardhat: {
+//       forking: {
+//         enabled: true,
+//         url: "https://eth-mainnet.alchemyapi.io/v2/IbkEKxpq0g2wcpd3XUqioRT4_-nsa0dv",
+//       }
+//     },
+//     goerli: {
+//       url: process.env.MAINNET_RPC,
+//       //@ts-ignore
+//       accounts: [process.env.PRIVATE_KEY1]
+//     }
+//   },
+//   etherscan: {
+//     apiKey: process.env.ETHERSCAN_APIKEY,
+//   },
+
+//   //gas: 2100000,
+// };
+
+// export default config;
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import dotenv from "dotenv";
@@ -7,13 +36,9 @@ const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
     hardhat: {
-      forking: {
-        enabled: true,
-        url: "https://eth-mainnet.alchemyapi.io/v2/IbkEKxpq0g2wcpd3XUqioRT4_-nsa0dv",
-      }
     },
     goerli: {
-      url: process.env.MAINNET_RPC,
+      url: process.env.GOERLI_RPC,
       //@ts-ignore
       accounts: [process.env.PRIVATE_KEY1]
     }
@@ -21,8 +46,6 @@ const config: HardhatUserConfig = {
   etherscan: {
     apiKey: process.env.ETHERSCAN_APIKEY,
   },
-
-  //gas: 2100000,
 };
 
 export default config;
